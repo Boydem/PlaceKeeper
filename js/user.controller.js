7d681caba43aa859c628edf33a1be438088fa28f
Boydem <noyemdahan@gmail.com>
2022-12-07 19:31:21 +0200

    set user service

---
+++ js/user.controller.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function onInit() {
+    renderUserPrefs()
+}
+
+function onSaveSettings(ev) {
+    ev.preventDefault()
+    const elFormInputs = document.querySelectorAll('form input')
+    let [userName, bgColor, txtColor, zoom, location] = elFormInputs
+    const newUserSettings = {
+        userName: userName.value,
+        bgColor: bgColor.value,
+        txtColor: txtColor.value,
+        zoom: zoom.value,
+        location: location.value
+    }
+    saveSettings(newUserSettings)
+    renderUserPrefs()
+}
+
+function renderUserPrefs() {
+
+    const user = getUser()
+    if (!user) return console.log('NO USER FOUND')
+    document.documentElement.style.setProperty('--bgc', `${user.bgColor}`);
+    document.documentElement.style.setProperty('--text', `${user.txtColor}`);
+
+}
--- js/user.service.js
+++ js/user.service.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const STORAGE_KEY = 'userDB'
+
+const gUser = _createUser()
+
+
+function saveSettings(newUserSettings) {
+    saveToStorage(STORAGE_KEY, newUserSettings)
+}
+
+function getUser() {
+    return loadFromStorage(STORAGE_KEY) || gUser
+}
+
+function _createUser() {
+    if (!loadFromStorage(STORAGE_KEY)) return {
+        id: makeId(),
+        userName: 'Guest',
+        bgColor: '#211C1C',
+        txtColor: '#FDDDDD',
+        zoom: 12,
+        location: '12,12'
+    }
+    else return loadFromStorage(STORAGE_KEY)
+}
